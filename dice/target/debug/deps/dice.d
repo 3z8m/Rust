d31794d719670ad0a7dd93f66722c64734303156
3z8m <89313312+3z8m@users.noreply.github.com>
2025-03-25 08:21:03 +0900

    Delete dice directory

--- dice/target/debug/deps/dice.d
+++
@@ -1,5 +0,0 @@
-C:\Users\M\Documents\MyProgram\Rust\chap2\dice\target\debug\deps\dice.exe: src\main.rs
-
-C:\Users\M\Documents\MyProgram\Rust\chap2\dice\target\debug\deps\dice.d: src\main.rs
-
-src\main.rs:
--- dice/target/debug/dice.d
+++
@@ -1,1 +0,0 @@
-C:\Users\M\Documents\MyProgram\Rust\chap2\dice\target\debug\dice.exe: C:\Users\M\Documents\MyProgram\Rust\chap2\dice\src\main.rs
